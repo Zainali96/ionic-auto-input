622f53c25bc4fdfa3db8279e291599c93b2a9116
Zain Ali <150120025+Zainali96@users.noreply.github.com>
2023-11-07 15:54:39 +0500

    Update auto-input.page.ts

--- auto-input.page.ts
+++ auto-input.page.ts
@@ -15,7 +15,8 @@ export class AutoInputPage implements OnInit {
   }
 
   checkInput(id:any){
-    if(id > 0 && this.inputs['_results'][id - 1].value.length == 0){
+    // Set focus on previous input
+    if(id > 0 && this.inputs['_results'][id - 1].value.length == 0){ 
       if(id != this.inputs['_results'].length && this.inputs['_results'][id ].value.length > 0){
         for(let i = 0; i<this.inputs['_results'].length; i++){
           this.inputs['_results'][i].value = '';
@@ -29,6 +30,7 @@ export class AutoInputPage implements OnInit {
         }
       }
     }else{
+      // Set focus on next input
       if(id < this.inputs['_results'].length){
         this.inputs['_results'][id].setFocus();
       }
